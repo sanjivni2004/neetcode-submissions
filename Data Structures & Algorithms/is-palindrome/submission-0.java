10e2ce72fe0cf97239d67e085d4ccd41716076bd
Sanjivni Katiyar <141104317+sanjivni2004@users.noreply.github.com>
2026-08-22 11:47:46 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l=0,r=s.length()-1;
+        while(l<r){
+            while(l<r && !alphanum(s.charAt(l))){
+                l++;
+            }
+            while(r>l && !alphanum(s.charAt(r))){
+                r--;
+            }
+            if(Character.toLowerCase(s.charAt(l))!=Character.toLowerCase(s.charAt(r))){
+                 return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    public boolean alphanum(char c){
+        return(c>='a'&& c<='z' || c>='A' && c<='Z'|| c>='0'&& c<='9');
+    }
+}
